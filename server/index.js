a294460e6247ff24269f30ba3a416d34794416c5
Anoop <anoopthottackad@gmail.com>
2023-06-10 13:10:02 +0530

    UserVerificationAtSignUp

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
+
 const url = "mongodb+srv://admi:Password@activity.5hkevpu.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 
@@ -10,19 +11,26 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/api/signup', async (req, res) => {
+  try {
+    await client.connect();
+    const collection = client.db("dataBase").collection("students");
     
-          await client.connect();
-          const db = client.db("dataBase");
-          const collection = db.collection("students");
-          const result = await collection.insertOne({name:req.body.name,pass:req.body.password});
-          console.log("Document inserted:", result.insertedId);
-      
-          await client.close();
-          console.log("Connection closed");
-    
-  res.json({ message: 'Sign up successful!' });
+    const existingUser = await collection.findOne({ name: req.body.name });
+    if (existingUser) {
+      res.json({ message: 'User already exists' });
+    } else {
+      await collection.insertOne({ name: req.body.name, pass: req.body.password });
+      res.json({ message: 'Sign up successful!' });
+    }
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    res.status(500).json({ message: 'Error connecting to MongoDB' });
+  } finally {
+    await client.close();
+  }
 });
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
