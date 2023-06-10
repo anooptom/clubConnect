15d43ebba223ad9c560ad78e4b3f1e93c21c8bea
Anoop <anoopthottackad@gmail.com>
2023-06-10 12:52:34 +0530

    DataInsertedToDatabase

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const cors = require('cors'); // Add this line
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const url = "mongodb+srv://admi:Password@activity.5hkevpu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
 
 app.use(bodyParser.json());
-
-// Add this middleware to enable CORS
 app.use(cors());
 
-app.post('/api/signup', (req, res) => {
-  console.log(req.body); // Display form data in the console
+app.post('/api/signup', async (req, res) => {
+    
+          await client.connect();
+          const db = client.db("dataBase");
+          const collection = db.collection("students");
+          const result = await collection.insertOne({name:req.body.name,pass:req.body.password});
+          console.log("Document inserted:", result.insertedId);
+      
+          await client.close();
+          console.log("Connection closed");
+    
   res.json({ message: 'Sign up successful!' });
 });
 
