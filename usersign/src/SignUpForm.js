defbceac526e2e7d2a692873b4fba0f3e5e2016c
Your Name <aswinn003@gmail.com>
2023-06-19 22:40:39 +0530

    signup

--- usersign/src/SignUpForm.js
+++ usersign/src/SignUpForm.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import "./LoginForm.css"
-import myImage from "./logo.jpg"
-import { useNavigate } from 'react-router';
+import React, { useState } from "react";
+import axios from "axios";
+import "./SignUp.css";
+import myImage from "./logo.jpg";
+import { useNavigate } from "react-router";
 
 const SignUpForm = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    name: '',
-    password: ''
+    name: "",
+    password: "",
   });
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -23,57 +23,45 @@ const SignUpForm = () => {
     console.log(formData); // Display form data in the console
 
     // Send form data to the server using Axios
-    axios.post(' http://localhost:3001/signup', formData)
-      .then(response => {
-        if(response.data.message === '1'){
-          navigate('/UserDashboard',{state:{Name: formData.name}});
-        }
-        else{
+    axios
+      .post(" http://localhost:3001/signup", formData)
+      .then((response) => {
+        if (response.data.message === "1") {
+          navigate("/UserDashboard", { state: { Name: formData.name } });
+        } else {
           alert("Existing User!,Login To Continue");
-          navigate('/user');
+          navigate("/user");
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   };
 
   return (
-    <div className="main">
-      
-    <div className="imghome">
-    <img src={myImage} alt="logo" />
+    <div className="signup">
+      <div className="main-signup">
+        <h2>Register</h2>
+
+        <form className="su-form" onSubmit={handleSubmit}>
+          <label className="su-label">Name : </label>
+          <input type="text"></input>
+          <label className="su-label">UID : </label>
+          <input type="text"></input>
+          <label className="su-label">Password: </label>
+          <input type="password"></input>
+          <label className="su-label">Club: </label>
+          <select className="select-su">
+            <option>1</option>
+            <option>2</option>
+          </select>
+          <br></br>
+          <button className="button-signup" type="submit">
+            Register
+          </button>
+        </form>
+      </div>
     </div>
-
-
-  <div className="formlogin">
-      
-      <h1>REGISTER</h1>
-
-      <form onSubmit={handleSubmit}>
-      <div className="input-container">
-          <label htmlFor="name">Username </label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
-      </div>
-
-      <div className="input-container">
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-      required/>
-      </div>
-
-      <div className="button-container">
-      <input type="submit" value='SIGN UP'/>
-      </div>
-      </form>
-
-  </div>
-</div>
   );
 };
 
